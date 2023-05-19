59a2864578430f64a16b54e8c9c9dfa5389fd450
ducvt-fabbi <ducvt@fabbi.io>
2023-05-19 17:37:04 +0700

    fix: reload screen list devices

--- src/screens/ListRegisteredDevicesView/ListRegisteredDevicesView.tsx
+++ src/screens/ListRegisteredDevicesView/ListRegisteredDevicesView.tsx
@@ -18,7 +18,7 @@ import {
 import type { ListRegisteredDevicesViewProps } from './types';
 import { DeviceItem } from './components';
 
-const ListRegisteredDevicesView = (props: ListRegisteredDevicesViewProps) => {
+const RootViewContainer = (props: ListRegisteredDevicesViewProps) => {
   const { headerProps, goBack } = props;
   const [listDevices, setListDevices] = useState<Array<DeviceInfo>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -49,29 +49,38 @@ const ListRegisteredDevicesView = (props: ListRegisteredDevicesViewProps) => {
     );
   };
 
-  const rootViewContainer = () => {
+  return (
+    <View style={styles.container}>
+      <Header {...headerProps} goBack={goBack} />
+      <Text style={styles.numberOfDevices}>
+        Thiết bị đã đăng ký ({listDevices?.length})
+      </Text>
+      <FlatList
+        data={listDevices}
+        keyExtractor={(_item, index) => index.toString()}
+        renderItem={renderItem}
+        style={styles.listConatiner}
+        contentContainerStyle={styles.contentContainerStyle}
+      />
+      {isLoading && <Loading />}
+    </View>
+  );
+};
+
+const ListRegisteredDevicesView = (props: ListRegisteredDevicesViewProps) => {
+  const renderRootView = () => {
     return (
-      <View style={styles.container}>
-        <Header {...headerProps} goBack={goBack} />
-        <Text style={styles.numberOfDevices}>
-          Thiết bị đã đăng ký ({listDevices?.length})
-        </Text>
-        <FlatList
-          data={listDevices}
-          keyExtractor={(_item, index) => index.toString()}
-          renderItem={renderItem}
-          style={styles.listConatiner}
-          contentContainerStyle={styles.contentContainerStyle}
-        />
-        {isLoading && <Loading />}
-      </View>
+      <RootViewContainer
+        goBack={props?.goBack}
+        headerProps={props?.headerProps}
+      />
     );
   };
 
   return (
     <NativeRouter>
       <Routes>
-        <Route path="/" Component={rootViewContainer} />
+        <Route path="/" Component={renderRootView} />
         <Route
           path="/deviceInfo/:device_id/:device_name/:secure_element/:biometric"
           Component={DeviceInfoView}
